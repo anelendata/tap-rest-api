e34690fdf49f0da52ed6d17179e330a11067a59e
Daigo Tanaka <daigo.tanaka@gmail.com>
2022-01-31 00:32:32 -0800

    support multiple streams in creating schemas (#26)

--- tap_rest_api/schema.py
+++ tap_rest_api/schema.py
@@ -72,47 +72,57 @@ def infer_schema(config, streams, out_catalog=True, add_tstamp=True):
     To fully support multiple streams, the catalog files must be consolidated
     but that is not supported in this function yet.
     """
-    # TODO: Support multiple streams specified by STREAM[]
-    tap_stream_id = streams[list(streams.keys())[0]].tap_stream_id
+    schemas = {}
+    for stream in list(streams.keys()):
+        tap_stream_id = streams[stream].tap_stream_id
 
-    params = get_init_endpoint_params(config, {}, tap_stream_id)
-    endpoint = get_endpoint(config["url"], tap_stream_id, params)
-    LOGGER.info("GET %s", endpoint)
-    auth_method = config.get("auth_method", "basic")
+        params = get_init_endpoint_params(config, {}, tap_stream_id)
 
-    headers = get_http_headers(config)
-    data = generate_request(tap_stream_id, endpoint, auth_method,
-                            headers,
-                            config.get("username"),
-                            config.get("password"))
+        url = config.get("urls", {}).get(tap_stream_id, config["url"])
+        endpoint = get_endpoint(url, tap_stream_id, params)
+        LOGGER.info("GET %s", endpoint)
+        auth_method = config.get("auth_method", "basic")
 
-    # In case the record is not at the root level
-    data = get_record_list(data, config.get("record_list_level"))
+        headers = get_http_headers(config)
+        data = generate_request(tap_stream_id, endpoint, auth_method,
+                                headers,
+                                config.get("username"),
+                                config.get("password"))
 
-    schema = getschema.infer_schema(data, config.get("record_level"))
-    if add_tstamp:
-        timestamp_format = {"type": ["null", "string"],
-                            "format": "date-time"}
-        schema["properties"][EXTRACT_TIMESTAMP] = timestamp_format
-        schema["properties"][BATCH_TIMESTAMP] = timestamp_format
+        # In case the record is not at the root level
+        data = get_record_list(data, config.get("record_list_level"))
+        schema = getschema.infer_schema(data, config.get("record_level"))
 
-    if not os.path.exists(config["schema_dir"]):
-        os.mkdir(config["schema_dir"])
+        if add_tstamp:
+            timestamp_format = {"type": ["null", "string"],
+                                "format": "date-time"}
+            schema["properties"][EXTRACT_TIMESTAMP] = timestamp_format
+            schema["properties"][BATCH_TIMESTAMP] = timestamp_format
 
-    with open(os.path.join(config["schema_dir"], tap_stream_id + ".json"),
-              "w") as f:
-        json.dump(schema, f, indent=2)
+        if not os.path.exists(config["schema_dir"]):
+            os.mkdir(config["schema_dir"])
 
-    if out_catalog:
-        schema["selected"] = True
-        catalog = {"streams": [{"stream": tap_stream_id,
-                                "tap_stream_id": tap_stream_id,
-                                "schema": schema
-                                }]}
-
-        if not os.path.exists(config["catalog_dir"]):
-            os.mkdir(config["catalog_dir"])
-
-        with open(os.path.join(config["catalog_dir"], tap_stream_id + ".json"),
+        schemas[tap_stream_id] = schema
+        with open(os.path.join(config["schema_dir"], tap_stream_id + ".json"),
                   "w") as f:
-            json.dump(catalog, f, indent=2)
+            json.dump(schema, f, indent=2)
+
+    if not out_catalog:
+        return
+
+    catalog = {"streams": []}
+    for stream in list(streams.keys()):
+        tap_stream_id = streams[stream].tap_stream_id
+        schema = schemas[tap_stream_id]
+        schema["selected"] = True
+        catalog["streams"].append({
+            "stream": tap_stream_id,
+            "tap_stream_id": tap_stream_id,
+            "schema": schema,
+        })
+
+    if not os.path.exists(config["catalog_dir"]):
+        os.mkdir(config["catalog_dir"])
+
+    with open(os.path.join(config["catalog_dir"], "catalog.json"), "w") as f:
+        json.dump(catalog, f, indent=2)
